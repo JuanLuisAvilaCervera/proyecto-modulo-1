b246db9699c53224d20031335bc4aefb5389c6ea
JuanLuisAvilaCervera <juanluisavilacervera44@gmail.com>
2025-02-18 00:39:32 +0100

    Scroll con dudas

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,24 @@
 
 
+
 window.onload = () => {
 
+    const html = document.querySelector('html');
+    const body = document.querySelector('body')
+    const height = Math.max( body.scrollHeight, body.offsetHeight, 
+                        html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+    generateVerticalScroll( body);
+    generateBackToTopButton(body);
+
+    window.addEventListener('scroll', () =>{
+        adjustVerticalScroll( height);
+    })
+
+
+}
+
+function generateVerticalScroll( body ){
     const scrollable = document.createElement("div");
     scrollable.setAttribute("id", "scrollable");
     scrollable.style.position = "fixed";
@@ -9,35 +26,44 @@ window.onload = () => {
     scrollable.style.top = 0;
     scrollable.style.width = 0;
 
-    const body = document.querySelector('body')
 
     body.prepend(scrollable);
-
-    window.addEventListener('scroll', () =>{
-        console.log("Scroll distance: " + window.scrollY);
-        
-
-        const html = document.querySelector('html');
-
-        const height = Math.max( body.scrollHeight, body.offsetHeight, 
-                       html.clientHeight, html.scrollHeight, html.offsetHeight );
-
-        console.log("Height: " + height);
-        const scrollpercentage = window.scrollY / height * 100;
-        scrollable.style.width = scrollpercentage+ "%"
-        scrollable.style.border = "solid 3px #08A6E4";
-        if(window.scrollY === 0){
-            scrollable.style.border = "none";
-        }
-    })
-
 }
 
-    //CREATE SCROLL PERCENTAGE
+function adjustVerticalScroll( height){
+    
 
-    //STYLES
+    const scrollpercentage = window.scrollY / height * 100;
+    scrollable.style.width = scrollpercentage+ "%"
+    scrollable.style.border = "solid 3px #08A6E4";
+    if(window.scrollY === 0){
+        scrollable.style.border = "none";
+    }
+}
 
-    //GET EVENT
+function generateBackToTopButton(body){
+    const backToTopDiv = document.createElement("div");
+    backToTopDiv.setAttribute("id", "backToTopDiv");
+    backToTopDiv.style = "width:100%; position:sticky; display:flex; justify-items: right;";
+    const backToTopButton = document.createElement("button");
+    backToTopButton.setAttribute("id", "backToTop");
+    backToTopButton.style = "position: fixed; bottom: 3rem; right: 3rem; border-radius: 50%; height: 5rem; width: 5rem; border: solid 3px black;"
+    backToTopDiv.append(backToTopButton);
 
-    //CHANGE SCROLL PERCENTAGE
+    body.append(backToTopDiv);
+
+    backToTopButton.addEventListener("click" , () => {
+        clickBackToTopButton(body);
+    })
+}
+
+function clickBackToTopButton( body ){
+    setTimeout(() => {
+        window.scrollTo({
+            top: 0,
+            behaviour: "smooth"
+        })
+    }, 200)
+}
+    
 
