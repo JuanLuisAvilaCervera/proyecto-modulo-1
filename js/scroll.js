c9e1b532921e27e182c937e977a995acfa9e0e38
JuanLuisAvilaCervera <juanluisavilacervera44@gmail.com>
2025-02-18 12:10:13 +0100

    buttonToTop terminado

--- js/scroll.js
+++ js/scroll.js
@@ -21,49 +21,35 @@ window.onload = () => {
 function generateVerticalScroll( body ){
     const scrollable = document.createElement("div");
     scrollable.setAttribute("id", "scrollable");
-    scrollable.style.position = "fixed";
-    scrollable.style.zIndex = 4;
-    scrollable.style.top = 0;
     scrollable.style.width = 0;
-
-
+    
     body.prepend(scrollable);
 }
 
 function adjustVerticalScroll( height){
-    
 
     const scrollpercentage = window.scrollY / height * 100;
     scrollable.style.width = scrollpercentage+ "%"
-    scrollable.style.border = "solid 3px #08A6E4";
     if(window.scrollY === 0){
-        scrollable.style.border = "none";
+        scrollable.style.display = "none";
+    }else{
+        scrollable.style.display = "block"
     }
 }
 
 function generateBackToTopButton(body){
-    const backToTopDiv = document.createElement("div");
-    backToTopDiv.setAttribute("id", "backToTopDiv");
-    backToTopDiv.style = "width:100%; position:sticky; display:flex; justify-items: right;";
-    const backToTopButton = document.createElement("button");
-    backToTopButton.setAttribute("id", "backToTop");
-    backToTopButton.style = "position: fixed; bottom: 3rem; right: 3rem; border-radius: 50%; height: 5rem; width: 5rem; border: solid 3px black;"
-    backToTopDiv.append(backToTopButton);
-
-    body.append(backToTopDiv);
+    const backToTopButton = document.getElementById('backToTop');
 
     backToTopButton.addEventListener("click" , () => {
-        clickBackToTopButton(body);
+        clickBackToTopButton();
     })
 }
 
-function clickBackToTopButton( body ){
-    setTimeout(() => {
-        window.scrollTo({
-            top: 0,
-            behaviour: "smooth"
-        })
-    }, 200)
+function clickBackToTopButton(){
+    setTimeout( () =>{
+        window.scrollTo({top:0, behavior: "smooth"})
+    }, 200);
+   
 }
     
 
